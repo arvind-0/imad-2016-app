20bec8377de9b28f6a5d6412293d65cdc5ddaaed
Arvind Srinivas.S <afkisanonymous@gmail.com>
2016-10-01 22:29:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.get('/article-one',function(req,res){
 res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
+app.get('/article-one',function(req,res){
+res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
